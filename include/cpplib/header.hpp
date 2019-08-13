41abecce9072aeb8b7a42844a87603f08412d6d2
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2019-08-13 17:54:42 -0300

    add bin exponentiation

--- include/cpplib/header.hpp
+++ include/cpplib/header.hpp
@@ -43,3 +43,28 @@ int gcd(int a, int b){
     if(a == 0) return b;
     return gcd(b%a, b);
 }
+
+/// Binary Exponentiation.
+int bexp(int a, int b){
+    int res = 1;
+    while(b > 0){
+        if(b & 1)
+            res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+
+/// Binary Exponatiation with module.
+int bexpm(int a, int b, int m = M){
+    a %= m;
+    int res = 1;
+    while(b > 0){
+        if(b & 1)
+            res = (res*a)%m;
+        a = (a*a)%m;
+        b >>= 1;
+    }
+    return res;
+}
