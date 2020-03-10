99bf182252e2a702bddc17a974460429540de315
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-03-10 20:46:16 -0300

    add set function to segtree

--- include/cpplib/data-structures/trees.hpp
+++ include/cpplib/data-structures/trees.hpp
@@ -14,38 +14,39 @@ class SegTree
 public:
     struct Node {
         SegTreeKind kind;
-        int val, lazy;
+        int value, lazy;
+        pair<int, bool> set;
 
         Node(const SegTreeKind kind) :
-            kind(kind), lazy(0)
+            kind(kind), lazy(0), set(0, false)
         {
             switch(kind){
                 case SegTreeKind::RMaxQ:
-                    val = -INF;
+                    value = -INF;
                     break;
                 case SegTreeKind::RMinQ:
-                    val = INF;
+                    value = INF;
                     break;
                 case SegTreeKind::RSumQ:
                 case SegTreeKind::RXorQ:
                 default:
-                    val = 0;
+                    value = 0;
                     break;
             }
         }
 
-        Node(const SegTreeKind kind, const int val, const int lazy = 0) :
-            kind(kind), val(val), lazy(lazy) {}
+        Node(const SegTreeKind kind, const int value, const int lazy = 0) :
+            kind(kind), value(value), lazy(lazy), set(0, false) {}
 
-        bool match(const int val) const
+        bool match(const int value) const
         {
             switch(kind){
                 case SegTreeKind::RMaxQ:
-                    return this->val >= val;
-                    // return this->val > val;
+                    return this->value >= value;
+                    // return this->value > value;
                 case SegTreeKind::RMinQ:
-                    return this->val <= val;
-                    // return this->val < val;
+                    return this->value <= value;
+                    // return this->value < value;
                 case SegTreeKind::RSumQ:
                 case SegTreeKind::RXorQ:
                 default:
@@ -56,15 +57,16 @@ public:
         static Node merge(const SegTreeKind kind, const Node lhs, const Node rhs)
         {
             assert(lhs.lazy == 0 and rhs.lazy == 0);
+            assert(!lhs.set.ss and !rhs.set.ss);
             switch(kind){
                 case SegTreeKind::RMaxQ:
-                    return Node(kind, max(lhs.val, rhs.val));
+                    return Node(kind, max(lhs.value, rhs.value));
                 case SegTreeKind::RMinQ:
-                    return Node(kind, min(lhs.val, rhs.val));
+                    return Node(kind, min(lhs.value, rhs.value));
                 case SegTreeKind::RSumQ:
-                    return Node(kind, lhs.val + rhs.val);
+                    return Node(kind, lhs.value + rhs.value);
                 case SegTreeKind::RXorQ:
-                    return Node(kind, lhs.val ^ rhs.val);
+                    return Node(kind, lhs.value ^ rhs.value);
                 default:
                     assert(false);
             }
@@ -80,9 +82,9 @@ public:
         build(0, arr_size-1, 0, arr);
     }
 
-    int find(const int val)
+    int find(const int value)
     {
-        return find(0, arr_size-1, 0, val);
+        return find(0, arr_size-1, 0, value);
     }
 
     int query(const int i)
@@ -94,7 +96,19 @@ public:
     int query(const int l, const int r)
     {
         assert(0 <= l and l <= r and r < arr_size);
-        return query(0, arr_size-1, l, r, 0).val;
+        return query(0, arr_size-1, l, r, 0).value;
+    }
+
+    void set(const int i, const int value)
+    {
+        assert(0 <= i and i < arr_size);
+        set(i, i, value);
+    }
+
+    void set(const int l, const int r, const int value)
+    {
+        assert(0 <= l and l <= r and r < arr_size);
+        set(0, arr_size-1, l, r, 0, value);
     }
 
     void update(const int i, const int delta)
@@ -119,18 +133,18 @@ private:
         return tree[pos] = Node::merge(kind, build(l, mid, 2*pos+1, arr), build(mid+1, r, 2*pos+2, arr));
     }
 
-    int find(const int l, const int r, const int pos, const int val)
+    int find(const int l, const int r, const int pos, const int value)
     {
         propagate(l, r, pos);
 
-        if(!tree[pos].match(val))
+        if(!tree[pos].match(value))
             return arr_size;
         else if(l == r)
             return l;
 
         int mid = (l + r)/2;
-        int ans = find(l, mid, 2*pos+1, val);
-        return ans != arr_size? ans : find(mid+1, r, 2*pos+2, val);
+        int ans = find(l, mid, 2*pos+1, value);
+        return ans != arr_size? ans : find(mid+1, r, 2*pos+2, value);
     }
 
     Node query(const int l, const int r, const int i, const int j, const int pos)
@@ -145,6 +159,24 @@ private:
 
         int mid = (l + r)/2;
         return Node::merge(kind, query(l, mid, i, j, 2*pos+1), query(mid+1, r, i, j, 2*pos+2));
+    }
+
+    Node set(const int l, const int r, const int i, const int j, const int pos, const int value)
+    {
+        propagate(l, r, pos);
+
+        if(l > r or l > j or r < i)
+            return tree[pos];
+
+        if(l >= i and r <= j){
+            tree[pos].set = {value, true};
+            // it's important to propagate before returning and Node::merge.
+            propagate(l, r, pos);
+            return tree[pos];
+        }
+
+        int mid = (l + r)/2;
+        return tree[pos] = Node::merge(kind, set(l, mid, i, j, 2*pos+1, value), set(mid+1, r, i, j, 2*pos+2, value));
     }
 
     Node update(const int l, const int r, const int i, const int j, const int pos, const int delta)
@@ -168,27 +200,55 @@ private:
     void propagate(int l, int r, int pos)
     {
         int lazy = tree[pos].lazy;
+        pair<int, bool> set = tree[pos].set;
 
-        if(lazy == 0)
+        if(lazy == 0 and !set.ss)
             return;
+
+        if(set.ss){
+            switch (kind){
+                case SegTreeKind::RMaxQ:
+                case SegTreeKind::RMinQ:
+                    tree[pos].value = set.ff;
+                    break;
+                case SegTreeKind::RSumQ:
+                    tree[pos].value = (r - l + 1)*set.ff;
+                    break;
+                case SegTreeKind::RXorQ:
+                    tree[pos].value = ((r - l + 1)%2)*set.ff;
+                    break;
+                default:
+                    break;
+            }
+        }
 
         switch (kind){
             case SegTreeKind::RMaxQ:
             case SegTreeKind::RMinQ:
-                tree[pos].val += lazy;
+                tree[pos].value += lazy;
                 break;
             case SegTreeKind::RSumQ:
-                tree[pos].val += (r - l + 1)*lazy;
+                tree[pos].value += (r - l + 1)*lazy;
                 break;
             case SegTreeKind::RXorQ:
             default:
                 break;
         }
+
         if(l != r){
+            if(set.ss){
+                tree[2*pos+1].set = set;
+                tree[2*pos+2].set = set;
+
+                tree[2*pos+1].lazy = 0;
+                tree[2*pos+2].lazy = 0;
+            }
             tree[2*pos+1].lazy += lazy;
             tree[2*pos+2].lazy += lazy;
         }
+
         tree[pos].lazy = 0;
+        tree[pos].set = {0, false};
     }
 
     SegTreeKind kind;
