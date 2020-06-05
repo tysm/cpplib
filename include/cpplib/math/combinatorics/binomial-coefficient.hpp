348d751d5f7dab139b1f683b9e4f0857c2a11f01
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-06-05 13:02:13 -0300

    [math] add [binomial-coefficient]

---
+++ include/cpplib/math/combinatorics/binomial-coefficient.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cpplib/stdinc.hpp>
+
+/**
+ * Binomial Coefficient.
+ *
+ * Computes C(n, k), the number
+ * in the n-th row and k-th column
+ * of the Pascal's triangle.
+ *
+ * Time Complexity: O(k).
+ * Space Complexity: O(k).
+ */
+uint binc(const uint n, const uint k){
+    assert(k <= n);
+    double res = 1;
+    for(uint i=1; i<=k; ++i)
+        res = res*(n - k + 1)/i;
+    return res + 0.01;
+}
