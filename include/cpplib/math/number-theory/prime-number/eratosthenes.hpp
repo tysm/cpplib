ef9d31d6ca41ffc593d58fb99cc11d3985120500
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-07-05 16:22:29 -0300

    [math] add segment sieve and refactor

--- include/cpplib/math/number-theory/prime-number/eratosthenes.hpp
+++ include/cpplib/math/number-theory/prime-number/eratosthenes.hpp
@@ -7,22 +7,22 @@
  * Computes the prime numbers until
  * an upper bound.
  *
- * Time Complexity: O(n*pow(log(n), 2)).
+ * Time Complexity: O(n*log(log(n))).
  * Space Complexity: O(n).
  * Where n is the upper bound.
  */
 class Sieve
 {
 public:
-    Sieve(const uint upper_bound) :
-        upper_bound(upper_bound), prime((upper_bound+1)/2, true)
+    Sieve(const uint sieve_u) :
+        sieve_u(sieve_u), prime((sieve_u+1)/2, true)
     {
         prime[0] = false;
-        for(uint i = 3; i*i <= upper_bound; i += 2){
+        for(uint i = 3; i*i <= sieve_u; i += 2){
             if(!prime[i/2])
                 continue;
-            for(uint j = i*i; j <= upper_bound; j += i){
-                if(j%2)
+            for(uint j = i*i; j <= sieve_u; j += i){
+                if(j & 1)
                     prime[j/2] = false;
             }
         }
@@ -36,22 +36,11 @@ public:
      */
     bool is_prime(const uint x) const
     {
-        assert(0 < x and x <= upper_bound);
-        return x%2? prime[x/2] : x == 2;
-    }
-
-    /**
-     * Returns the upper bound.
-     *
-     * Time Complexity: O(1).
-     * Space Complexity: O(1).
-     */
-    size_t size() const
-    {
-        return upper_bound;
+        assert(x <= sieve_u);
+        return x & 1? prime[x/2] : x == 2;
     }
 
 private:
-    uint upper_bound;
-    vector<bool> prime;
+    uint sieve_u; // sieve upper bound.
+    vector<bool> prime; // is_prime as boolean array.
 };
--- include/cpplib/math/number-theory/prime-number/gries-misra.hpp
+++ include/cpplib/math/number-theory/prime-number/gries-misra.hpp
@@ -10,39 +10,76 @@
  *
  * Time Complexity: O(n).
  * Space Complexity: O(n).
- * Where n is the upper bound.
+ * Where n is the sieve upper bound.
  */
 class Sieve
 {
 public:
-    Sieve(const uint upper_bound) :
-        upper_bound(upper_bound), min_prime_factor(upper_bound+1)
+    Sieve(const uint sieve_u) :
+        sieve_u(sieve_u), spf(sieve_u+1)
     {
-        for(uint i = 2; i <= upper_bound; ++i){
-            if(!min_prime_factor[i]){
-                min_prime_factor[i] = i;
+        for(uint i = 2; i <= sieve_u; ++i){
+            if(!spf[i]){
+                spf[i] = i;
                 primes.emplace_back(i);
             }
             for(uint p : primes){
-                if(p > min_prime_factor[i] or i*p > upper_bound)
+                if(p > spf[i] or i*p > sieve_u)
                     break;
-                min_prime_factor[i*p] = p;
+                spf[i*p] = p;
             }
+        }
+    }
+
+    /**
+     * Computes the prime numbers in the range
+     * [segment_l, segment_u].
+     *
+     * Time Complexity: O(n).
+     * Space Complexity: O(n).
+     * Where n is the size of the segment range (i.e. segment_u - segment_l + 1).
+     */
+    void segment(const uint segment_l, const uint segment_u)
+    {
+        assert(segment_l <= segment_u and ceil(sqrt(segment_u)) <= sieve_u);
+        tie(this->segment_l, this->segment_u) = make_tuple(segment_l, segment_u);
+
+        uint n = segment_u - segment_l + 1, lim = sqrt(segment_u);
+        segment_spf.assign(n, 0);
+        segment_primes.clear();
+
+        for(uint p : primes){
+            if(p > lim)
+                break;
+            for(uint i = (p - segment_l%p)%p; i < n; i += p){
+                if(!segment_spf[i] and i + segment_l > p)
+                    segment_spf[i] = p;
+            }
+        }
+
+        for(uint i = segment_l; i <= segment_u; ++i){
+            if(i < 2 or segment_spf[i - segment_l])
+                continue;
+            segment_spf[i - segment_l] = i;
+            segment_primes.emplace_back(i);
         }
     }
 
     /**
      * Computes the prime factorization of x.
      *
+     * Note: it's not viable to factorize numbers
+     * > sieve_u.
+     *
      * Time Complexity: O(log(x)).
      * Space Complexity: O(log(x)).
      */
     vector<pair<uint, uint> > factorization(uint x) const
     {
-        assert(0 < x and x <= upper_bound);
+        assert(0 < x and x <= sieve_u);
         vector<pair<uint, uint> > factors;
-        for(; x > 1; x /= min_prime_factor[x]){
-            uint p = min_prime_factor[x];
+        for(; x > 1; x /= spf[x]){
+            uint p = spf[x];
             if(factors.empty() or factors.back().first != p)
                 factors.emplace_back(p, 1);
             else
@@ -54,20 +91,43 @@ public:
     /**
      * Checks if a number is prime.
      *
+     * Note: x must be <= sieve_u or in the
+     * segment range.
+     *
      * Time Complexity: O(1).
      * Space Complexity: O(1).
      */
     bool is_prime(const uint x) const
     {
-        assert(0 < x and x <= upper_bound);
-        return x <= 1? false : min_prime_factor[x] == x;
+        assert(x <= sieve_u or segment_l <= x and x <= segment_u);
+        if(x <= sieve_u)
+            return x <= 1? false : spf[x] == x;
+        else
+            return segment_spf[x - segment_l] == x;
     }
 
     /**
-     * Return the computed prime numbers.
+     * Returns the smallest prime factor of x.
+     *
+     * Note: x must be <= sieve_u or in the
+     * precomputed segment range.
+     *
+     * Time Complexity: O(1).
+     * Space Complexity: O(1).
+     */
+    uint smallest_prime_factor(const uint x) const
+    {
+        assert(1 < x and (x <= sieve_u or segment_l <= x and x <= segment_u));
+        return x <= sieve_u? spf[x] : segment_spf[x - segment_l];
+    }
+
+    /**
+     * Returns the computed prime numbers <=
+     * sieve_u.
      *
      * Time Complexity: O(n).
      * Space Complexity: O(n).
+     * Where n is the sieve upper bound.
      */
     vector<uint> prime_numbers() const
     {
@@ -75,17 +135,21 @@ public:
     }
 
     /**
-     * Returns the upper bound.
+     * Returns the computed prime numbers in the
+     * segment range.
      *
-     * Time Complexity: O(1).
-     * Space Complexity: O(1).
+     * Time Complexity: O(n).
+     * Space Complexity: O(n).
+     * Where n is the size of the segment range (i.e. segment_u - segment_l + 1).
      */
-    size_t size() const
+    vector<uint> segment_prime_numbers() const
     {
-        return upper_bound;
+        return segment_primes;
     }
 
 private:
-    uint upper_bound;
-    vector<uint> min_prime_factor, primes;
+    uint sieve_u; // sieve upper bound.
+    vector<uint> spf, primes; // smallest prime factor and the primes <= sieve_u.
+    uint segment_l = 0, segment_u = 0; // segment upper and lower bounds.
+    vector<uint> segment_spf, segment_primes; // segment smallest prime factor and the primes in [segment_u, segment_lower].
 };
