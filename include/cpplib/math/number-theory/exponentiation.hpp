7074186f8dbca5d89318cbb4e555f53919a5f5aa
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-07-05 02:36:19 -0300

    [math] refactor [exponentiation] and [progression]

--- include/cpplib/math/number-theory/exponentiation.hpp
+++ include/cpplib/math/number-theory/exponentiation.hpp
@@ -9,27 +9,9 @@
  * Time Complexity: O(log(e)).
  * Space Complexity: O(1).
  */
-int bexp(int b, uint e){
-    int res = 1;
-    for(; e > 0; e >>= 1){
-        if(e & 1)
-            res *= b;
-        b *= b;
-    }
-    return res;
-}
-
-/**
- * Binary Exponentiation.
- *
- * Note: this is the same algorithm as the
- * one above, but with b as double.
- *
- * Time Complexity: O(log(e)).
- * Space Complexity: O(1).
- */
-double bexp(double b, uint e){
-    double res = 1;
+template<typename T>
+T exp(T b, uint e){
+    T res = 1;
     for(; e > 0; e >>= 1){
         if(e & 1)
             res *= b;
--- include/cpplib/math/progression.hpp
+++ include/cpplib/math/progression.hpp
@@ -11,29 +11,13 @@
  * Time Complexity: O(1).
  * Space Complexity: O(1).
  */
-int sum_ap(const int a, const int d, const int n){
+template<typename T>
+T sum_ap(const T a, const T d, const uint n){
     assert(n > 0);
-    int aux = 2*a + d*(n-1);
-    if(aux%2 == 0)
-        return (aux/2)*n;
-    return aux*(n/2);
-}
-
-/**
- * Sum of Arithmetic Progression.
- *
- * Note: this is the same algorithm as the
- * one above, but with double parameters.
- *
- * Time Complexity: O(1).
- * Space Complexity: O(1).
- */
-double sum_ap(const double a, const double d, const int n){
-    assert(n > 0);
-    double aux = 2*a + d*(n-1);
+    T aux = 2*a + d*(n-1);
     if(n%2 == 0)
         return aux*(n/2);
-    return (aux*n)/2;
+    return (aux/2)*n;
 }
 
 /**
@@ -45,30 +29,12 @@ double sum_ap(const double a, const double d, const int n){
  * Time Complexity: O(log(n)).
  * Space Complexity: O(1).
  */
-int sum_gp(const int a, const int r, const int n){
+template<typename T>
+T sum_gp(const T a, const T r, const uint n){
     assert(n > 0);
     if(r == 1)
         return a*n;
-    int aux = 1 - bexp(r, n);
-    if(a%(1 - r) == 0)
-        return (a/(1 - r))*aux;
-    return a*(aux/(1- r));
-}
-
-/**
- * Sum of Geometric Progression.
- *
- * Note: this is the same algorithm as the
- * one above, but with double parameters.
- *
- * Time Complexity: O(log(n)).
- * Space Complexity: O(1).
- */
-double sum_gp(const double a, const double r, const int n){
-    assert(n > 0);
-    if(r == 1)
-        return a*n;
-    return (a*(1 - bexp(r, n)))/(1 - r);
+    return (a*(1 - exp(r, n)))/(1- r);
 }
 
 /**
