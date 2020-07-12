f4dd06f831daff6fea737a4f052796d6b5446010
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-07-12 16:13:21 -0300

    [data-structure] keep lowercase [point]

--- include/cpplib/data-structure/geometry/point.hpp
+++ include/cpplib/data-structure/geometry/point.hpp
@@ -4,14 +4,14 @@
 
 template<typename T,
 typename enable_if<is_arithmetic<T>::value, uint>::type = 0>
-struct Point {
+struct point {
     T x, y, z;
 
-    explicit Point(const T x = 0, const T y = 0, const T z = 0) :
+    explicit point(const T x = 0, const T y = 0, const T z = 0) :
         x(x), y(y), z(z) {}
 
     template<typename T1>
-    Point(const Point<T1> &p) :
+    point(const point<T1> &p) :
         x(p.x), y(p.y), z(p.z) {}
 
     // Component sum - O(1).
@@ -21,13 +21,13 @@ struct Point {
     }
 
     // Negative vector - O(1).
-    Point operator-() const
+    point operator-() const
     {
-        return Point(-x, -y, -z);
+        return point(-x, -y, -z);
     }
 
     // Unit vector - O(1).
-    Point<double> operator~() const
+    point<double> operator~() const
     {
         return (*this)/norm(*this);
     }
@@ -40,7 +40,7 @@ struct Point {
 
     // Vector addiction assignment - O(1).
     template<typename T1>
-    Point &operator+=(const Point<T1> &rhs)
+    point &operator+=(const point<T1> &rhs)
     {
         x += rhs.x;
         y += rhs.y;
@@ -50,7 +50,7 @@ struct Point {
 
     // Vector subtraction assignment - O(1).
     template<typename T1>
-    Point &operator-=(const Point<T1> &rhs)
+    point &operator-=(const point<T1> &rhs)
     {
         x -= rhs.x;
         y -= rhs.y;
@@ -60,7 +60,7 @@ struct Point {
 
     // Vector cross product assignment - O(1).
     template<typename T1>
-    Point &operator^=(const Point<T1> &rhs)
+    point &operator^=(const point<T1> &rhs)
     {
         tie(x, y, z) = make_tuple(y*rhs.z - z*rhs.y, z*rhs.x - x*rhs.z, x*rhs.y - y*rhs.x);
         return *this;
@@ -68,7 +68,7 @@ struct Point {
 
     // Vector projection assignment - O(1).
     template<typename T1>
-    Point &operator>>=(const Point<T1> &rhs)
+    point &operator>>=(const point<T1> &rhs)
     {
         auto res = ((*this)*rhs/(!rhs))*rhs;
         tie(x, y, z) = make_tuple(res.x, res.y, res.z);
@@ -77,13 +77,13 @@ struct Point {
 
     // Vector rejection assignment - O(1).
     template<typename T1>
-    Point &operator<<=(const Point<T1> &rhs)
+    point &operator<<=(const point<T1> &rhs)
     {
         return *this -= (*this >> rhs);
     }
 
     // Scalar multiplication assignment - O(1).
-    Point &operator*=(const T rhs)
+    point &operator*=(const T rhs)
     {
         x *= rhs;
         y *= rhs;
@@ -92,7 +92,7 @@ struct Point {
     }
 
     // Scalar division assignment - O(1).
-    Point &operator/=(const T rhs)
+    point &operator/=(const T rhs)
     {
         x /= rhs;
         y /= rhs;
@@ -102,157 +102,157 @@ struct Point {
 
     // Vector addiction - O(1).
     template<typename T1>
-    friend auto operator+(const Point &lhs, const Point<T1> &rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator+(const point &lhs, const point<T1> &rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T, T1>::type> res(lhs);
+        point<typename common_type<T, T1>::type> res(lhs);
         return res += rhs;
     }
 
     // Vector subtraction - O(1).
     template<typename T1>
-    friend auto operator-(const Point &lhs, const Point<T1> &rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator-(const point &lhs, const point<T1> &rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T, T1>::type> res(lhs);
+        point<typename common_type<T, T1>::type> res(lhs);
         return res -= rhs;
     }
 
     // Vector dot product - O(1).
     template<typename T1>
-    friend auto operator*(const Point &lhs, const Point<T1> &rhs) -> typename common_type<T, T1>::type
+    friend auto operator*(const point &lhs, const point<T1> &rhs) -> typename common_type<T, T1>::type
     {
         return lhs.x*rhs.x + lhs.y*rhs.y + lhs.z*rhs.z;
     }
 
     // Vector cross product - O(1).
     template<typename T1>
-    friend auto operator^(const Point &lhs, const Point<T1> &rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator^(const point &lhs, const point<T1> &rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T, T1>::type> res(lhs);
+        point<typename common_type<T, T1>::type> res(lhs);
         return res ^= rhs;
     }
 
     // Vector projection - O(1).
     template<typename T1>
-    friend Point<double> operator>>(const Point &lhs, const Point<T1> &rhs)
+    friend point<double> operator>>(const point &lhs, const point<T1> &rhs)
     {
-        Point<double> res(lhs);
+        point<double> res(lhs);
         return res >>= rhs;
     }
 
     // Vector rejection - O(1).
     template<typename T1>
-    friend Point<double> operator<<(const Point &lhs, const Point<T1> &rhs)
+    friend point<double> operator<<(const point &lhs, const point<T1> &rhs)
     {
-        Point<double> res(lhs);
+        point<double> res(lhs);
         return res <<= rhs;
     }
 
     // Scalar multiplication - O(1).
     template<typename T1,
     typename enable_if<is_arithmetic<T1>::value, uint>::type = 0>
-    friend auto operator*(const Point &lhs, const T1 rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator*(const point &lhs, const T1 rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T, T1>::type> res(lhs);
+        point<typename common_type<T, T1>::type> res(lhs);
         return res *= rhs;
     }
 
     // Scalar multiplication - O(1).
     template<typename T1,
     typename enable_if<is_arithmetic<T1>::value, uint>::type = 0>
-    friend auto operator*(const T1 lhs, const Point &rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator*(const T1 lhs, const point &rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T1, T>::type> res(rhs);
+        point<typename common_type<T1, T>::type> res(rhs);
         return res *= lhs;
     }
 
     // Scalar division - O(1).
     template<typename T1,
     typename enable_if<is_arithmetic<T1>::value, uint>::type = 0>
-    friend auto operator/(const Point &lhs, const T1 rhs) -> Point<typename common_type<T, T1>::type>
+    friend auto operator/(const point &lhs, const T1 rhs) -> point<typename common_type<T, T1>::type>
     {
-        Point<typename common_type<T, T1>::type> res(lhs);
+        point<typename common_type<T, T1>::type> res(lhs);
         return res /= rhs;
     }
 
     // Vector equal to - O(1).
     template<typename T1>
-    friend bool operator==(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator==(const point &lhs, const point<T1> &rhs)
     {
         return tie(lhs.x, lhs.y, lhs.z) == tie(rhs.x, rhs.y, rhs.z);
     }
 
     // Vector not equal to - O(1).
     template<typename T1>
-    friend bool operator!=(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator!=(const point &lhs, const point<T1> &rhs)
     {
         return !(lhs == rhs);
     }
 
     // Vector less than - O(1).
     template<typename T1>
-    friend bool operator<(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator<(const point &lhs, const point<T1> &rhs)
     {
         return tie(lhs.x, lhs.y, lhs.z) < tie(rhs.x, rhs.y, rhs.z);
     }
 
     // Vector greater than - O(1).
     template<typename T1>
-    friend bool operator>(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator>(const point &lhs, const point<T1> &rhs)
     {
         return rhs < lhs;
     }
 
     // Vector less than or equal to - O(1).
     template<typename T1>
-    friend bool operator<=(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator<=(const point &lhs, const point<T1> &rhs)
     {
         return !(lhs > rhs);
     }
 
     // Vector greater than or equal to - O(1).
     template<typename T1>
-    friend bool operator>=(const Point &lhs, const Point<T1> &rhs)
+    friend bool operator>=(const point &lhs, const point<T1> &rhs)
     {
         return !(lhs < rhs);
     }
 
     // Vector triple product - O(1).
     template<typename T1, typename T2>
-    friend auto triple(const Point &a, const Point<T1> &b, const Point<T2> &c) -> typename common_type<T, T1, T2>::type
+    friend auto triple(const point &a, const point<T1> &b, const point<T2> &c) -> typename common_type<T, T1, T2>::type
     {
         return a*(b^c);
     }
 
     // Vector norm - O(1).
-    friend double norm(const Point &p)
+    friend double norm(const point &p)
     {
         return sqrt(!p);
     }
 
     // Scalar projection - O(1).
     template<typename T1>
-    friend double projection(const Point &a, const Point<T1> &b)
+    friend double projection(const point &a, const point<T1> &b)
     {
         return a*b/norm(b);
     }
 
     // Scalar rejection - O(1).
     template<typename T1>
-    friend double rejection(const Point &a, const Point<T1> &b)
+    friend double rejection(const point &a, const point<T1> &b)
     {
         return norm(a - (a >> b));
     }
 
     // Point distance - O(1).
     template<typename T1>
-    friend double distance(const Point &a, const Point<T1> &b)
+    friend double distance(const point &a, const point<T1> &b)
     {
         return norm(a - b);
     }
 
     // Angle between two vectors - O(1).
     template<typename T1>
-    friend double angle(const Point &a, const Point<T1> &b)
+    friend double angle(const point &a, const point<T1> &b)
     {
         auto aux = a*b/norm(a)/norm(b);
         return aux > 1? acos(1) : (aux < -1? acos(-1) : acos(aux));
@@ -260,14 +260,14 @@ struct Point {
 
     // Angle between three points - O(1).
     template<typename T1, typename T2>
-    friend double angle(const Point &a, const Point<T1> &b, const Point<T2> &c)
+    friend double angle(const point &a, const point<T1> &b, const point<T2> &c)
     {
         return angle(a-b, c-b); // angle between b->a and b->c.
     }
 
     // Angle counterclockwise orientation between two vectors - O(1).
     template<typename T1>
-    friend tuple<int, int, int> orientation(const Point &a, const Point<T1> &b)
+    friend tuple<int, int, int> orientation(const point &a, const point<T1> &b)
     {
         auto aux = a^b;
         //  1 means counterclockwise.
@@ -278,13 +278,13 @@ struct Point {
     }
 
     // Vector string conversion - O(1).
-    friend string to_string(const Point &p)
+    friend string to_string(const point &p)
     {
         return '(' + to_string(p.x) + ' ' + to_string(p.y) + ' ' + to_string(p.z) + ')';
     }
 
     // Vector output - O(1).
-    friend ostream &operator<<(ostream &lhs, const Point &rhs)
+    friend ostream &operator<<(ostream &lhs, const point &rhs)
     {
         return lhs << to_string(rhs);
     }
