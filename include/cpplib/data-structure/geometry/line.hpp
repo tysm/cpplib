0f55b9d85a2d950f78134c3ab99667bbb046d8c4
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2020-07-19 20:30:09 -0300

    [data-structures] count modifications [line & segment]

--- include/cpplib/data-structure/geometry/line.hpp
+++ include/cpplib/data-structure/geometry/line.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <cpplib/stdinc.hpp>
 #include <cpplib/data-structure/geometry/point.hpp>
-#include <cpplib/data-structure/geometry/segment.hpp>
 
 template<typename T>
 struct line {
@@ -31,20 +30,6 @@ struct line {
         return p + k*v;
     }
 
-    // Point lies on line check - O(1).
-    template<typename T1>
-    bool count(const point<T1> &q) const
-    {
-        return is_degenerate()? q == p : parallel((q - p), v);
-    }
-
-    // Segment lies on line check - O(1).
-    template<typename T1>
-    bool count(const segment<T1> &s) const
-    {
-        return count(s.a) and count(s.b);
-    }
-
     // Line equivalent to - O(1).
     template<typename T1>
     friend bool operator==(const line &lhs, const line<T1> &rhs)
@@ -52,7 +37,7 @@ struct line {
         if(lhs.is_degenerate() and rhs.is_degenerate())
             return lhs.p == rhs.p;
         else if(!lhs.is_degenerate() and !rhs.is_degenerate())
-            return parallel(lhs, rhs) and lhs.count(rhs.p);
+            return parallel(lhs, rhs) and count(lhs, rhs.p);
         return false;
     }
 
@@ -63,118 +48,62 @@ struct line {
         return !(lhs == rhs);
     }
 
-    // Segment-line coplanarity check - O(1).
+    // Point lies on line check - O(1).
     template<typename T1>
-    friend bool coplanar(const segment<T1> &s, const line &l)
+    friend bool count(const line &l, const point<T1> &p)
     {
-        return coplanar(s.b - s.a, l.v, l.p - s.a);
+        return l.is_degenerate()? p == l.p : parallel((p - l.p), l.v);
     }
 
-    // Line-line coplanarity check - O(1).
+    // Line coplanarity check - O(1).
     template<typename T1>
     friend bool coplanar(const line<T1> &r, const line &l)
     {
         return coplanar(r.v, l.v, l.p - r.p);
     }
 
-    // Segment-line parallelism check - O(1).
-    template<typename T1>
-    friend bool parallel(const segment<T1> &s, const line &l)
-    {
-        return parallel(s.b - s.a, l.v);
-    }
-
-    // Line-line parallelism check - O(1).
+    // Line parallelism check - O(1).
     template<typename T1>
     friend bool parallel(const line<T1> &r, const line &l)
     {
         return parallel(r.v, l.v);
     }
 
-    // Segment-line concurrency check - O(1).
-    template<typename T1>
-    friend bool concurrent(const segment<T1> &s, const line &l)
-    {
-        return coplanar(s, l) and !parallel(s, l);
-    }
-
-    // Line-line concurrency check - O(1).
+    // Line concurrency check - O(1).
     template<typename T1>
     friend bool concurrent(const line<T1> &r, const line &l)
     {
         return coplanar(r, l) and !parallel(r, l);
     }
 
-    // Segment-line orthogonality check - O(1).
-    template<typename T1>
-    friend bool orthogonal(const segment<T1> &s, const line &l)
-    {
-        return perpendicular(s.b - s.a, l.v);
-    }
-
-    // Line-line orthogonality check - O(1).
+    // Line orthogonality check - O(1).
     template<typename T1>
     friend bool orthogonal(const line<T1> &r, const line &l)
     {
         return perpendicular(r.v, l.v);
     }
 
-    // Segment-line perpendicularity check - O(1).
-    template<typename T1>
-    friend bool perpendicular(const segment<T1> &s, const line &l)
-    {
-        return coplanar(s, l) and orthogonal(s, l);
-    }
-
-    // Line-line perpendicularity check - O(1).
+    // Line perpendicularity check - O(1).
     template<typename T1>
     friend bool perpendicular(const line<T1> &r, const line &l)
     {
         return coplanar(r, l) and orthogonal(r, l);
     }
 
-    // Segment-line intersection check - O(1).
-    template<typename T1>
-    friend bool intersect(const segment<T1> &s, const line &l)
-    {
-        line<T1> r = {s.a, s.b - s.a};
-        if(r == l)
-            return true;
-        else if(s.is_degenerate() and l.count(s.a))
-            return true;
-        else if(l.is_degenerate() and s.count(l.p))
-            return true;
-        return concurrent(r, l) and s.count(intersection(r, l).p);
-    }
-
-    // Line-line intersection check - O(1).
+    // Line intersection check - O(1).
     template<typename T1>
     friend bool intersect(const line<T1> &r, const line &l)
     {
         if(r == l)
             return true;
-        else if(r.is_degenerate() and l.count(r.p))
+        else if(r.is_degenerate() and count(l, r.p))
             return true;
-        else if(l.is_degenerate() and r.count(l.p))
+        else if(l.is_degenerate() and count(r, l.p))
             return true;
-        return  concurrent(r, l);
+        return concurrent(r, l);
     }
 
-    // Segment-line intersection - O(1).
-    template<typename T1>
-    friend segment<double> intersection(const segment<T1> &s, const line &l)
-    {
-        assert(intersect(s, l));
-        line<T1> r = {s.a, s.b - s.a};
-        if(r == l or s.is_degenerate())
-            return s;
-        else if(l.is_degenerate())
-            return {l.p, l.p};
-        auto aux = intersection(r, l);
-        return {aux.p, aux.p};
-    }
-
-    // Line-line intersection - O(1).
+    // Line intersection - O(1).
     template<typename T1>
     friend line<double> intersection(const line<T1> &r, const line &l)
     {
@@ -188,7 +117,7 @@ struct line {
         return {r.at(k*orientation(rl, l.v, r.v^l.v)), point<double>()};
     }
 
-    // Point-line distance - O(1).
+    // Point distance - O(1).
     template<typename T1>
     friend double distance(const point<T1> &p, const line &l)
     {
@@ -197,7 +126,7 @@ struct line {
         return sqrt((double)squared_norm((p - l.p)^l.v)/squared_norm(l.v));
     }
 
-    // Line-line distance - O(1).
+    // Line distance - O(1).
     template<typename T1>
     friend double distance(const line<T1> &r, const line &l)
     {
@@ -218,13 +147,6 @@ struct line {
         return res;
     }
 
-    // Segment projection onto line - O(1).
-    template<typename T1>
-    friend segment<double> projection(const segment<T1> &s, const line &l)
-    {
-        return {projection(s.a, l), projection(s.b, l)};
-    }
-
     // Line projection onto line - O(1).
     template<typename T1>
     friend line<double> projection(const line<T1> &r, const line &l)
@@ -238,13 +160,6 @@ struct line {
     friend point<double> reflection(const point<T1> &p, const line &l)
     {
         return 2*projection(p, l) - p;
-    }
-
-    // Segment reflection over line - O(1).
-    template<typename T1>
-    friend segment<double> reflection(const segment<T1> &s, const line &l)
-    {
-        return {reflection(s.a, l), reflection(s.b, l)};
     }
 
     // Line reflection over line - O(1).
--- include/cpplib/data-structure/geometry/segment.hpp
+++ include/cpplib/data-structure/geometry/segment.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cpplib/stdinc.hpp>
 #include <cpplib/data-structure/geometry/point.hpp>
+#include <cpplib/data-structure/geometry/line.hpp>
 
 template<typename T>
 struct segment {
@@ -22,16 +23,6 @@ struct segment {
         return a == b;
     }
 
-    // Point lies on segment check - O(1).
-    template<typename T1>
-    bool count(const point<T1> &p) const
-    {
-        if(is_degenerate())
-            return p == a;
-        auto pa = a - p, pb = b - p;
-        return parallel(pa, pb) and pa*pb <= EPS;
-    }
-
     // Segment equal to - O(1).
     template<typename T1>
     friend bool operator==(const segment &lhs, const segment<T1> &rhs)
@@ -46,72 +37,150 @@ struct segment {
         return !(lhs == rhs);
     }
 
-    // Segment coplanarity check - O(1).
+    // Point lies on segment check - O(1).
+    template<typename T1>
+    friend bool count(const segment &s, const point<T1> &p)
+    {
+        if(s.is_degenerate())
+            return p == s.a;
+        auto pa = s.a - p, pb = s.b - p;
+        return parallel(pa, pb) and pa*pb <= EPS;
+    }
+
+    // Segment lies on segment check - O(1).
+    template<typename T1>
+    friend bool count(const segment &s, const segment<T1> &r)
+    {
+        return count(s, r.a) and count(s, r.b);
+    }
+
+    // Segment lies on line check - O(1).
+    template<typename T1>
+    friend bool count(const line<T1> &l, const segment &s)
+    {
+        return count(l, s.a) and count(l, s.b);
+    }
+
+    // Segment-segment coplanarity check - O(1).
     template<typename T1>
     friend bool coplanar(const segment<T1> &r, const segment &s)
     {
         return coplanar(r.b - r.a, s.b - s.a, s.a - r.a);
     }
 
-    // Segment parallelism check - O(1).
+    // Segment-line coplanarity check - O(1).
+    template<typename T1>
+    friend bool coplanar(const segment &s, const line<T1> &l)
+    {
+        return coplanar(s.b - s.a, l.v, l.p - s.a);
+    }
+
+    // Segment-segment parallelism check - O(1).
     template<typename T1>
     friend bool parallel(const segment<T1> &r, const segment &s)
     {
         return parallel(r.b - r.a, s.b - s.a);
     }
 
-    // Segment concurrency check - O(1).
+    // Segment-line parallelism check - O(1).
+    template<typename T1>
+    friend bool parallel(const segment &s, const line<T1> &l)
+    {
+        return parallel(s.b - s.a, l.v);
+    }
+
+    // Segment-segment concurrency check - O(1).
     template<typename T1>
     friend bool concurrent(const segment<T1> &r, const segment &s)
     {
         return coplanar(r, s) and !parallel(r, s);
     }
 
-    // Segment orthogonality check - O(1).
+    // Segment-line concurrency check - O(1).
+    template<typename T1>
+    friend bool concurrent(const segment &s, const line<T1> &l)
+    {
+        return coplanar(s, l) and !parallel(s, l);
+    }
+
+    // Segment-segment orthogonality check - O(1).
     template<typename T1>
     friend bool orthogonal(const segment<T1> &r, const segment &s)
     {
         return perpendicular(r.b - r.a, s.b - s.a);
     }
 
-    // Segment perpendicularity check - O(1).
+    // Segment-line orthogonality check - O(1).
+    template<typename T1>
+    friend bool orthogonal(const segment &s, const line<T1> &l)
+    {
+        return perpendicular(s.b - s.a, l.v);
+    }
+
+    // Segment-segment perpendicularity check - O(1).
     template<typename T1>
     friend bool perpendicular(const segment<T1> &r, const segment &s)
     {
         return coplanar(r, s) and orthogonal(r, s);
     }
 
-    // Segment intersection check - O(1).
+    // Segment-line perpendicularity check - O(1).
+    template<typename T1>
+    friend bool perpendicular(const segment &s, const line<T1> &l)
+    {
+        return coplanar(s, l) and orthogonal(s, l);
+    }
+
+    // Segment-segment intersection check - O(1).
     template<typename T1>
     friend bool intersect(const segment<T1> &r, const segment &s)
     {
-        if(r.count(s.a) or r.count(s.b) or s.count(r.a) or s.count(r.b))
+        if(count(r, s.a) or count(r, s.b) or count(s, r.a) or count(s, r.b))
             return true; // covers r == s, degenerate cases and partial intersection.
-        else if(concurrent(s, r)){ // actually (line) intersection code.
-            auto rs = s.a - r.a, rv = r.b - r.a, sv = s.b - s.a;
-            double k = sqrt((double)squared_norm(rs^sv)/squared_norm(rv^sv));
-            auto p = r.a + k*orientation(rs, sv, rv^sv)*rv;
-            return r.count(p) and s.count(p);
-        }
-        return false;
+        line<T> l = {s.a, s.b - s.a};
+        return intersect(r, l) and count(s, intersection(r, l).a);
     }
 
-    // Segment intersection - O(1).
+    // Segment-line intersection check - O(1).
+    template<typename T1>
+    friend bool intersect(const segment &s, const line<T1> &l)
+    {
+        line<T> r = {s.a, s.b - s.a};
+        if(r == l)
+            return true;
+        else if(s.is_degenerate() and count(l, s.a))
+            return true;
+        else if(l.is_degenerate() and count(s, l.p))
+            return true;
+        return concurrent(r, l) and count(s, intersection(r, l).p);
+    }
+
+    // Segment-segment intersection - O(1).
     template<typename T1>
     friend segment<double> intersection(const segment<T1> &r, const segment &s)
     {
         assert(intersect(r, s));
-        if(r == s or r.is_degenerate())
+        if(r == s or r.is_degenerate() or count(s, r))
             return r;
-        else if(s.is_degenerate())
+        else if(s.is_degenerate() or count(r, s))
             return s;
-        else if(concurrent(s, r)){ // actually (line) intersection code.
-            auto rs = s.a - r.a, rv = r.b - r.a, sv = s.b - s.a;
-            double k = sqrt((double)squared_norm(rs^sv)/squared_norm(rv^sv));
-            auto p = r.a + k*orientation(rs, sv, rv^sv)*rv;
-            return {p, p};
-        }
-        return {s.count(r.a)? r.a : r.b, r.count(s.a)? s.a : s.b};
+        else if(concurrent(s, r))
+            return intersection(r, line<T1>(s.a, s.b - s.a));
+        return {count(s, r.a)? r.a : r.b, count(r, s.a)? s.a : s.b};
+    }
+
+    // Segment-line intersection - O(1).
+    template<typename T1>
+    friend segment<double> intersection(const segment &s, const line<T1> &l)
+    {
+        assert(intersect(s, l));
+        line<T1> r = {s.a, s.b - s.a};
+        if(r == l or s.is_degenerate())
+            return s;
+        else if(l.is_degenerate())
+            return {l.p, l.p};
+        auto aux = intersection(r, l);
+        return {aux.p, aux.p};
     }
 
     // Point-segment distance - O(1).
@@ -123,7 +192,7 @@ struct segment {
             return distance(p, s.a);
         else if(bp*ab >= -EPS)
             return distance(p, s.b);
-        return sqrt((double)squared_norm(ap^ab)/squared_norm(ab)); // same as distance(p, line(s.a, ab)).
+        return distance(p, line<T>(s.a, ab));
     }
 
     // Closest point in segment - O(1).
@@ -135,7 +204,21 @@ struct segment {
             return s.a;
         else if(bp*ab >= -EPS)
             return s.b;
-        return s.a + ab*((double)(ap*ab)/squared_norm(ab)); // same as projection(p, line(s.a, ab)).
+        return projection(p, line<T>(s.a, ab));
+    }
+
+    // Segment projection onto line - O(1).
+    template<typename T1>
+    friend segment<double> projection(const segment &s, const line<T1> &l)
+    {
+        return {projection(s.a, l), projection(s.b, l)};
+    }
+
+    // Segment reflection over line - O(1).
+    template<typename T1>
+    friend segment<double> reflection(const segment &s, const line<T1> &l)
+    {
+        return {reflection(s.a, l), reflection(s.b, l)};
     }
 
     // String conversion - O(1).
