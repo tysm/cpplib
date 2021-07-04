67f99f014275fb179f76718db2c527039b4233d6
Thalles Yan Santos Medrado <ysm.thalles@gmail.com>
2021-07-04 01:35:18 -0300

    [adt] update includes [geoinc]

--- include/cpplib/adt/geoinc.hpp
+++ include/cpplib/adt/geoinc.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include <cpplib/adt/circle.hpp>
 #include <cpplib/adt/line.hpp>
 #include <cpplib/adt/point.hpp>
+#include <cpplib/adt/polygon.hpp>
 #include <cpplib/adt/ray.hpp>
 #include <cpplib/adt/segment.hpp>
+#include <cpplib/adt/triangle.hpp>
